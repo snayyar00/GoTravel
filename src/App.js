3992faed6b7153ee29c7a28c437c3413cf03df57
Sidharth <sidharth15nayyar@gmail.com>
2020-08-23 22:57:20 -0700

    products not working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+
 import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
